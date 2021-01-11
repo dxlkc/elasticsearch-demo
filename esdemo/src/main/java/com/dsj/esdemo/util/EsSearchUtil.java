faa4b4213192cfb1f267f87938437f1b55f89b5e
dxlkc <43257580+dxlkc@users.noreply.github.com>
2021-01-11 21:13:12 +0800

    修该查询语句，添加部分未实现功能

--- esdemo/src/main/java/com/dsj/esdemo/util/EsSearchUtil.java
+++ esdemo/src/main/java/com/dsj/esdemo/util/EsSearchUtil.java
@@ -1,12 +1,14 @@
 package com.dsj.esdemo.util;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.log4j.Log4j2;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.*;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -33,23 +35,18 @@ public class EsSearchUtil {
         boolean result = false;
         SearchRequest searchRequest = new SearchRequest(index);
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("title", title);
-        sourceBuilder.query(matchQueryBuilder);
+        QueryBuilder queryBuilder = QueryBuilders
+                .boolQuery()
+                .filter(QueryBuilders.termQuery("title.keyword", title));
+        sourceBuilder.query(queryBuilder);
         searchRequest.source(sourceBuilder);
 
         try {
             SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
             SearchHits hits = searchResponse.getHits();
             SearchHit[] searchHits = hits.getHits();
-            // 为了精确匹配
             if (searchHits.length > 0) {
-                for (SearchHit hit : searchHits) {
-                    Map<String, Object> sourceAsMap = hit.getSourceAsMap();
-                    if (hit.getScore() >= 1 && sourceAsMap.get("title").equals(title)){
-                        result = true;
-                        break;
-                    }
-                }
+                result = true;
             }
         } catch (IOException e) {
             log.warn("isDocumentTitleExist发生异常! index:{}, title:{} \n {}", index, title, e.getMessage());
@@ -58,5 +55,44 @@ public class EsSearchUtil {
         }
 
         return result;
+    }
+
+    /**
+     * 模糊查询，查询所有index中title、content和搜索信息匹配的文档（考虑和下面一个合并）
+     * 分页查询，按照score降序排序
+     * @param searchInfo 用户搜索信息
+     * @param page       需要返回的当前页数
+     * @param pageSize   当前页内容数量
+     * @return
+     */
+    public JSONObject fuzzySearchByTitleAndContent(String searchInfo, String page, String pageSize) {
+
+        return null;
+    }
+
+    /**
+     * 模糊查询，查询指定index中title、content和搜索信息匹配的文档
+     * 分页查询，按照score降序排序
+     * @param searchInfo 用户搜索信息
+     * @param index      指定的索引名称
+     * @param page       需要返回的当前页数
+     * @param pageSize   当前页内容数量
+     * @return
+     */
+    public JSONObject fuzzySearchByIndexAndTitleAndContent(String searchInfo, String index, String page, String pageSize) {
+
+        return null;
+    }
+
+    /**
+     * 全量查询，查询所有收集的文档
+     * 分页查询，（按照索引名称降序排序）
+     * @param page  需要返回的当前页数
+     * @param pageSize 当前页内容数量
+     * @return
+     */
+    public JSONObject fullSearch(String page, String pageSize) {
+
+        return null;
     }
 }
